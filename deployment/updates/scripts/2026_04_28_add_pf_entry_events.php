11956d68cc24d73743984a0f3a521e7fa541fc10
yossipapi <yossi.papiashvili@kaltura.com>
2026-04-29 18:19:08 +0300

    Change param name

--- deployment/updates/scripts/2026_04_28_add_pf_entry_events.php
+++ deployment/updates/scripts/2026_04_28_add_pf_entry_events.php
@@ -5,13 +5,13 @@
 require_once (__DIR__ . '/../../bootstrap.php');
 
 $script = realpath(dirname(__FILE__) . '/../../../') . '/tests/standAloneClient/exec.php';
-$scheduleEventNotificationUpdate = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2026_04_28_add_kafka_entry_pf_notifications.xml");
+$pfEntryNotifications = realpath(dirname(__FILE__) . "/../../updates/scripts/xml/notifications/2026_04_28_add_kafka_entry_pf_notifications.xml");
 
 
-if(!file_exists($scheduleEventNotificationUpdate))
+if(!file_exists($pfEntryNotifications))
 {
 	KalturaLog::err("Missing update script file");
 	return;
 }
 
-passthru("php $script $scheduleEventNotificationUpdate");
+passthru("php $script $pfEntryNotifications");
